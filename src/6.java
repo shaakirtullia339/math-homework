4a254728b1bd27fd949473bcefe5d0b5b12831b3
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-03-11 03:12:18 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+  public static void main(String[] args) {
+    int x = (int) (Math.random() * 10);
+    int y = (int) (Math.random() * 10);
+    System.out.println("The sum of " + x + " and " + y + " is " + (x + y));
+  }
+}
