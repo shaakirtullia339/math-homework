a2e5c4487600922b240ed102a1ad0ade7c42a373
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-03-12 07:02:56 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MathHomework {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+  }
+}
