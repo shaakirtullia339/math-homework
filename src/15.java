8af83ca2d04869b20f318c8b47085bc33a8a7720
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-03-20 23:09:34 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Add two numbers using addition operator
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Example 2: Subtract two numbers using subtraction operator
+        double difference = 7.5 - 2.0;
+        System.out.println("Difference between 7.5 and 2.0 is: " + difference);
+    }
+}
