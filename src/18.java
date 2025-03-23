cc1132ff14a113f4d2cec0d100dbaab5591cd035
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-03-23 22:16:06 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example calculation and output using Java's built-in operations
+        double result = 5.0 * 3 + 2.0 / 4 - 1.0;
+        System.out.println("Result: " + result);
+    }
+}
