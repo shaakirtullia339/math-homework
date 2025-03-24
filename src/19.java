03073984f8eaf572f939f5590d8273e9c196dcc6
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-03-24 00:18:17 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 10.2;
+        
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
