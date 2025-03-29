ee83bc6449ed040da8222a6baa7d406fc7d58e73
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-03-29 18:47:35 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to demonstrate mathematical operations
+        double a = 5;
+        double b = 3.14;
+        double c = 7;
+        
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+        System.out.println("The difference between " + c + " and " + b + " is: " + (c - b));
+        System.out.println("The product of " + c + " and " + b + " is: " + (c * b));
+        System.out.println("The quotient of " + a + " and " + b + " is: " + (a / b));
+    }
+}
