0c749960d1749814f0091464489d3531d2a28716
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-04-08 05:08:28 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int result = 5 + 3;
+        System.out.println(result);
+    }
+}
