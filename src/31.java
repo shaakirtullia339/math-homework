ef3e7ecd81173e8c1aa4278c2e53962a1945c778
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-04-24 15:21:18 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,16 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        int addition = 5 + 3;
+        System.out.println("Addition result: " + addition);
+
+        int subtraction = 10 - 2;
+        System.out.println("Subtraction result: " + subtraction);
+
+        int multiplication = 4 * 7;
+        System.out.println("Multiplication result: " + multiplication);
+
+        int division = 16 / 8;
+        System.out.println("Division result: " + division);
+    }
+}
