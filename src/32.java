5bef25c77040418a942e1b99e5a9332098b7408c
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-04-25 02:01:13 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 10.5;
+        System.out.println("The sum of " + x + " and " + y + " is: " + (x + y));
+        System.out.println("The product of " + x + " and " + y + " is: " + (x * y));
+    }
+}
