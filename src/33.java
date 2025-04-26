bf4fb648d747fefd5786a1abc079ceeb73447d8f
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-04-26 01:12:22 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        double sum = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is: " + sum);
+    }
+}
