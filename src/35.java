ff339476a249eb33c289a238e1fd9d7de5362aee
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-05-17 06:54:49 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical calculation: 5 + 3 = 8
+        int result = 5 + 3;
+        System.out.println("The result is: " + result);
+    }
+}
