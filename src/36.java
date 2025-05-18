a94239bf15d3c50c9b12f6c9f196819deddb6be5
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-05-18 20:51:26 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14159;
+
+        System.out.println("x is " + x);
+        System.out.println("y squared is " + (int)y * y);
+    }
+}
