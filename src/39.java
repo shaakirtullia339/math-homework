71b26e537e280424ea908f4999302b22dbedd77e
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-05-24 13:36:42 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = 3.7;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + (num1 - num2));
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + (num1 * num2));
+    }
+}
