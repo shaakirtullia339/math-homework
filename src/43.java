42eaff3e8290ceae076258ee6030bad3b0e75b4a
shaakirtullia339 <shaakirtullia339@gmail.com>
2025-05-28 05:31:48 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example calculation: x^2 - 3x + 2
+        double x = 4;
+        System.out.println("Result of " + x + "^2 - 3*"+x+ "+2 is " + (x*x-x+2));
+    }
+}
